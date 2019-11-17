9bd513998f504932134d4def60a064dfafaadb24
Massiaux Alexis <alexis.massiaux@worldline.com>
2019-11-17 13:03:11 +0100

    week1 test1 param

--- week1/exo103/WEB-INF/classes/web/BagServlet.java
+++ week1/exo103/WEB-INF/classes/web/BagServlet.java
@@ -9,28 +9,69 @@ import javax.servlet.http.*;
 
 // TODO: this class should extends something to be a usable servlet.
 // TODO: add an annotation here to map your servlet on an URL.
-public class BagServlet {
+@WebServlet("/bag")
+public class BagServlet extends HttpServlet{
 	
 	Bag myBag = new Bag();
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+		res.setContentType( "text/html" );
 		PrintWriter out = res.getWriter();
 		
 		// TODO : print a html form using printwriter.
+		
+		out.println( "<HTML>" );
+		out.println( "<HEAD>");
+		out.println( "<TITLE>Page generee par une servlet</TITLE>" );
+		out.println( "</HEAD>" );
+		out.println( "<BODY>" );
+		out.println( "<H1>Bonjour</H1>" );
+		out.println("<form action=\"\" method=\"post\">");
+		out.println("<label for=\"ref\">Reference \t : </label>");
+		out.println("<input type=\"text\" name=\"ref\" id=\"ref\">");
+		out.println("<br/><br/>");
+		out.println("<label for=\"qty\">Quantity \t : </label>");
+		out.println("<input type=\"text\" name=\"qty\" id=\"qty\">");
+		out.println("<br/><br/>");
+		out.println("<input type=\"submit\" value=\"Subscribe!\">");
+		out.println("</form>");
+		out.println( "</BODY>" );
+		out.println( "</HTML>" );
+		
 	}
 
 	
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+		res.setContentType( "text/html" );
 		PrintWriter out = res.getWriter();
 		
-		// TODO : Get parameters, check null
+		String reference = req.getParameter("ref");
+		String quantity = req.getParameter("qty");
 		
-		// TODO : print reference and quantity
-
+		// TODO : Get parameters, check null
+		if(reference == null || quantity == null || reference.equals("") || quantity.equals("") || !isNumeric(quantity)) {
+			res.setStatus(400);
+		}else{
+			// TODO : print reference and quantity
+			out.println("<p>"+reference+"</p>");
+			out.println("<p>"+quantity+"</p>");
+		}
 	}
+	
+	public static boolean isNumeric(final String str) {
+
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
 	
 	
 	
